9d01820b51f880f670c41cc26137216dbf08f6e9
Krapaud <mur.mickael@gmail.com>
2026-01-28 08:39:01 +0100

    Fix parseInt base from 16 to 10 for correct size parsing

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 /* The Size variable was not declared with const */
-const size = parseInt(process.argv[2], 16)
+const size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
